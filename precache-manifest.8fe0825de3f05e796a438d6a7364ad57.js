dc12115db38e8787b17d91b65d5e22cdcf9dd0ad
Kevin <kev.saintigny.schmidt@gmail.com>
2020-06-27 22:35:40 +0200

    Updates

---
+++ precache-manifest.8fe0825de3f05e796a438d6a7364ad57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e88b5c8271d251ac56b0694d443471",
+    "url": "/react--movie-selection/index.html"
+  },
+  {
+    "revision": "88767a8f198eea5fb2e9",
+    "url": "/react--movie-selection/static/css/main.ea74ad3a.chunk.css"
+  },
+  {
+    "revision": "28b8c54591441159fad6",
+    "url": "/react--movie-selection/static/js/2.ee634351.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react--movie-selection/static/js/2.ee634351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88767a8f198eea5fb2e9",
+    "url": "/react--movie-selection/static/js/main.9cf590f1.chunk.js"
+  },
+  {
+    "revision": "5fe13e1533b643a38b4e",
+    "url": "/react--movie-selection/static/js/runtime-main.d1e55a5f.js"
+  }
+]);
